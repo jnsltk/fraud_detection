9fbaa88bccbf4c06650b2c3c7fc78fcef0106a2c
janos <janos@student.chalmers.se>
2024-11-18 17:59:25 +0100

    #4 Set up backend with django and connect to db

---
+++ fraud_detection_backend/detector/views.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+
+@require_http_methods(['GET'])
+def test(request):
+    return JsonResponse({'message': 'Hello World!'})
+
+# Create your views here.
