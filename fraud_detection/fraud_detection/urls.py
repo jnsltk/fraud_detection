0130d635e639e50ad7d86f3a64a835606cd50f45
janos <janos@student.chalmers.se>
2024-11-27 09:45:16 +0100

    #10 Remove Vue.js frontend project, rename Django project
    We decided to switch to a Django-based frontend, making the Vue.js setup unnecessary.
    This commit removes all associated files and dependencies from the repo,
    and renames the Django project to remove the word "backend" to reflect
    these changes.

--- fraud_detection/fraud_detection/urls.py
+++ fraud_detection/fraud_detection/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for fraud_detection_backend project.
+URL configuration for fraud_detection project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
