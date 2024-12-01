6e0d43d563e8235a062398b6fa14e2fd32d0393f
janos <janos@student.chalmers.se>
2024-12-02 00:46:18 +0100

    #13 Finish Manage models page with dummy data

--- fraud_detection/dashboard/views.py
+++ fraud_detection/dashboard/views.py
@@ -11,4 +11,20 @@ def index(request):
     return render(request, 'dashboard/index.html', context=context)
 
 def manage_models(request):
-    return render(request, 'dashboard/manage_models.html')
+    context = {}
+    # Define dummy data for now
+    models = []
+    for i in range(10):
+        models.append({
+            'version': 'v3.19',
+            'date_created': '2024.11.30',
+            'created_by': 'Bob McRoberts',
+            'dataset_size': '100000',
+            'score': '99.4',
+            'deployed': False
+        })
+    # Set first value to deployed
+    models[0]['deployed'] = True
+
+    context.update({'models': models})
+    return render(request, 'dashboard/manage_models.html', context=context)
