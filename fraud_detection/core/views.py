fa1fa51cdb465679406f3af1bb0ab82b1e9b0fb5
Omid Khodaparast <guskhodom@student.gu.se>
2024-12-01 22:46:33 +0100

    #7 Fix styling of the home page. there are still fixes left

--- fraud_detection/core/views.py
+++ fraud_detection/core/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
 
 def home_page(request):
-    context = {
-        'message': "hellooo"
-    }
-
-    return render(request, 'home_page/index.html', context=context)
+    return render(request, 'home_page/index.html')
