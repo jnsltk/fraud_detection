c5ebbabff522b0302961a068e01201587efe9b28
janos <janos@student.chalmers.se>
2024-12-05 10:26:25 +0100

    #17 Modify manage_models view to simulate model creation

--- fraud_detection/dashboard/views.py
+++ fraud_detection/dashboard/views.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 from django.contrib.auth.decorators import login_required
@@ -7,6 +8,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 
+from detector.models import FraudDetectionModel
 from dashboard.forms import NewModelForm
 
 
@@ -17,6 +19,7 @@ def index(request):
     }
     return render(request, 'dashboard/index.html', context=context)
 
+
 @require_http_methods(['GET', 'POST'])
 def manage_models(request):
     if request.method == "POST":
@@ -24,10 +27,28 @@ def manage_models(request):
         if form.is_valid():
             start_date = form.cleaned_data['start_date']
             end_date = form.cleaned_data['end_date']
-            # Call start model training pipeline, passing in the selected dates
+            # Call start model training pipeline here, passing in the selected dates
+
+            # Simulate model creation
             print(start_date, end_date)
-            # simulate model training time
-            time.sleep(5)
+            # Simulate model training time
+            time.sleep(3)
+            random_data = os.urandom(1024)
+            dummy_model = FraudDetectionModel(
+                version="v3.2-dummy",
+                date_created="2024-12-04",
+                dataset_size=100000,
+                training_data_start_date=start_date,
+                training_data_end_date=end_date,
+                score=98.02,
+                detailed_performance={'f1-score': '98.02'},
+                metadata={'important_data': 'yes'},
+                model_file=random_data,
+                is_deployed=True,
+                created_by=request.user
+            )
+            dummy_model.save()
+
             context = {
                 'desc': 'Success!',
                 'message': 'Congratulations! You\'ve just trained a new model! Click \'Deploy\' if you want to use it.'
@@ -58,9 +79,13 @@ def train_model_form(request):
     return render(request, 'dashboard/train_model_form.html', {
         'form': form,
     })
+
+
 def cancel_model_form(request):
     return HttpResponse(
         """<div id="dialog"></div>"""
     )
+
+
 def train_model_result(request):
     return render(request, 'dashboard/train_model_result.html')
