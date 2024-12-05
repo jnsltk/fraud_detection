2786fe5bf52a3305968f6e2b9377c003031fd6d5
janos <janos@student.chalmers.se>
2024-12-05 19:11:40 +0100

    #17 Implement model deployment, model deletion, improve model training simulation

--- fraud_detection/dashboard/views.py
+++ fraud_detection/dashboard/views.py
@@ -1,3 +1,4 @@
+import json
 import os
 import time
 
@@ -10,6 +11,7 @@ from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods
 from django.db.models.functions import Concat
 from django.db.models import Value
+from django.db.models import Max
 
 from detector.models import FraudDetectionModel
 from dashboard.forms import NewModelForm
@@ -22,6 +24,7 @@ def index(request):
         'users_num': User.objects.count,
     }
     return render(request, 'dashboard/index.html', context=context)
+
 
 @login_required
 @staff_member_required
@@ -36,12 +39,16 @@ def manage_models(request):
 
             # Simulate model creation
             try:
-                print(start_date, end_date)
+                # Bump up model version
+                highest_version = FraudDetectionModel.objects.aggregate(max_version=Max('version'))
+                version_num = highest_version.get('max_version')
+                version_bump = f"v{round(float(version_num[1:-6]) + 0.1, 2)}-dummy"
+
                 # Simulate model training time
                 time.sleep(3)
                 random_data = os.urandom(1024)
                 dummy_model = FraudDetectionModel(
-                    version="v3.2-dummy",
+                    version=version_bump,
                     date_created=date.today(),
                     dataset_size=100000,
                     training_data_start_date=start_date,
@@ -67,31 +74,15 @@ def manage_models(request):
         return render(request, 'dashboard/train_model_result.html', context=context)
     else:
         context = {}
-        #Query model metadata from database
-        model_metadata = list(
-            FraudDetectionModel.objects.annotate(
-                created_by_full_name=Concat(
-                    'created_by__first_name',
-                    Value(' '),
-                    'created_by__last_name'
-                )
-            ).values(
-                'version',
-                'date_created',
-                'created_by_full_name',
-                'dataset_size',
-                'score',
-                'is_deployed'
-            )
-        )
-
-        # Set first value to deployed
-        model_metadata[0]['is_deployed'] = True
+        # Query model metadata from database
+        model_metadata = load_models()
 
         context.update({'models': model_metadata})
         return render(request, 'dashboard/manage_models.html', context=context)
 
 
+@login_required
+@staff_member_required
 def train_model_form(request):
     form = NewModelForm()
     return render(request, 'dashboard/train_model_form.html', {
@@ -99,11 +90,66 @@ def train_model_form(request):
     })
 
 
+@login_required
+@staff_member_required
 def cancel_model_form(request):
     return HttpResponse(
         """<div id="dialog"></div>"""
     )
 
 
+@login_required
+@staff_member_required
 def train_model_result(request):
     return render(request, 'dashboard/train_model_result.html')
+
+
+@login_required
+@staff_member_required
+def deploy_model(request):
+    selected_model_id = request.POST.get('deploy_id')
+    selected_model = FraudDetectionModel.objects.get(id=selected_model_id)
+    try:
+        deployed_model = FraudDetectionModel.objects.get(is_deployed=True)
+        if deployed_model != selected_model:
+            deployed_model.is_deployed = False
+            deployed_model.save()
+    except Exception as e:
+        print('No deployed model found\n', e)
+    selected_model.is_deployed = True
+    selected_model.save()
+    context = {
+        'models': load_models()
+    }
+    return render(request, 'dashboard/model_table.html', context=context)
+
+@login_required
+@staff_member_required
+def delete_model(request):
+    selected_model_id = request.POST.get('delete_id')
+    selected_model = FraudDetectionModel.objects.get(id=selected_model_id)
+    selected_model.delete()
+
+    context = {
+        'models': load_models()
+    }
+    return render(request, 'dashboard/model_table.html', context=context)
+
+def load_models():
+    return list(
+        FraudDetectionModel.objects.annotate(
+            created_by_full_name=Concat(
+                'created_by__first_name',
+                Value(' '),
+                'created_by__last_name'
+            )
+        ).values(
+            'id',
+            'version',
+            'date_created',
+            'created_by_full_name',
+            'dataset_size',
+            'score',
+            'is_deployed'
+        ).order_by('-version', '-date_created')
+    )
