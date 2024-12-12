479ff9e8da8f84b0f665b118f558a356594d5db8
Omid Khodaparast <guskhodom@student.gu.se>
2024-12-12 14:16:31 +0100

    #28 Add links and buttons to allow navigation through the whole web app for staff users and admin users

--- fraud_detection/core/views.py
+++ fraud_detection/core/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.http import require_GET, require_POST, require_http
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .form import RegisterForm, LoginForm, ChangePasswordForm, MyProfileForm
+from .forms import RegisterForm, LoginForm, ChangePasswordForm, MyProfileForm
 
 
 @require_GET
